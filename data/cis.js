c953b3069b895b8a77b7bf892568da27b998f18d
幾個秋Te-Wei Chiang（小威老師談電腦） <tewei0328@gmail.com>
2021-11-17 18:55:43 +0800

    Update cis.js

--- data/cis.js
+++ data/cis.js
@@ -10,6 +10,8 @@ function data1() {
   {q:"1.僕は投資家ではなく、", a:"我不是投資者", img:"https://i.imgur.com/ozPPgfi.jpeg"},
   {q:"2.その本質はどちらかといえば~", a:"本質是相當~", img:"https://i.imgur.com/stetmNV.jpg"}, 
   ];
+  alert("data1()");  
+  console.log("data1()");  
   return cisdata;
 }
 
