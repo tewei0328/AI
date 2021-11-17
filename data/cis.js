f3cbb0170e7590fb106ed72273015aa5aa2f1c0e
幾個秋Te-Wei Chiang（小威老師談電腦） <tewei0328@gmail.com>
2021-11-17 10:38:05 +0800

    Update cis.js

--- data/cis.js
+++ data/cis.js
@@ -1,14 +1,11 @@
-var cis0=[
+cis0=[
   {q:"1.僕は投資家ではなく、", a:"我不是投資者", img:"https://i.imgur.com/ozPPgfi.jpeg"},
 
   {q:"2.その本質はどちらかといえば~", a:"本質是相當~", img:"https://i.imgur.com/stetmNV.jpg"}, 
   ];
   
-var cis1=[
+cis1=[
   {q:"3.僕は投資家ではなく、", a:"我不是投資者", img:"https://i.imgur.com/ozPPgfi.jpeg"},
 
   {q:"4.その本質はどちらかといえば~", a:"本質是相當~", img:"https://i.imgur.com/stetmNV.jpg"}, 
   ];
-
-var obj = Object.assign({}, cis0, cis1);
-console.log(obj)
